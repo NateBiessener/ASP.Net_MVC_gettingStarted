ec82986280cd6efb51259073e04d0a7f1c4b2d69
Nate Biessener <nathaniel.biessener@gmail.com>
2016-12-07 10:30:16 -0600

    controller hello world

---
+++ FirstMVC/FirstMVC/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string Detail()
+        {
+            return "Hello from Controller!";
+        }
+    }
+}
--- FirstMVC/FirstMVC/Controllers/HomeController.cs
+++ FirstMVC/FirstMVC/Controllers/HomeController.cs
@@ -7,17 +7,11 @@ using System.Web.Mvc.Ajax;
 
 namespace FirstMVC.Controllers
 {
-	public class HomeController : Controller
+	public class FirstController : Controller
 	{
-		public ActionResult Index()
+		public string Detail()
 		{
-			var mvcName = typeof(Controller).Assembly.GetName();
-			var isMono = Type.GetType("Mono.Runtime") != null;
-
-			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
-			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
-
-			return View();
+			return "Hello from controller!";
 		}
 	}
 }
